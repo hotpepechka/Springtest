88239d3f4dbf47e94407f10970937370087adce2
Kuznetsov Stanislav <SMikhKuznetsov@sberbank.ru>
2024-01-12 20:25:18 +0300

    first work with class/interface and spring DI

--- src/main/java/com/example/test/controllers/HelloController.java
+++ src/main/java/com/example/test/controllers/HelloController.java
@@ -1,18 +1,27 @@
 package com.example.test.controllers;
 
 
+import com.example.test.service.InMemoryUserService;
+import com.example.test.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/")
     public String Hello(){
+
         return
                 "<!DOCTYPE html>"+
                         "<html>"+
                         "	<head><title>Hello world!</title></head>"+
-                        "	<body>Hello world!</body>"+
+                        "	<body>Hello world! " + userService.getName() + " </body>"+
                         "</html>"
                 ;
     }
